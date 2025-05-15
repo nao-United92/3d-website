2007caa5eb08f9abb66a21edd91c96f2ae95f02e
nao-United92 <naoyauver@icloud.com>
2025-05-16 00:50:24 +0900

    delete App.css and add Tag for Gradient Image & Blur Effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main>
+      {/* Gradient Image */}
+      <img
+        className="absolute top-0 right-0 opacity-60 -z-10"
+        src="/public/gradient.png"
+        alt="Gradient-img"
+      />
+
+      {/* Blur Effect */}
+      <div className="h-0 w-[40rem] absolute top[20%] right-[-5%] shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10"></div>
+    </main>
+  );
 }
