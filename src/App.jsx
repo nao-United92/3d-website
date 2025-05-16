fdca6485c80c1d7f5e4435c1783a26245c0efc17
nao-United92 <naoyauver@icloud.com>
2025-05-16 23:14:37 +0900

    add Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Header from './components/Header';
+
 export default function App() {
   return (
     <main>
@@ -10,6 +12,8 @@ export default function App() {
 
       {/* Blur Effect */}
       <div className="h-0 w-[40rem] absolute top[20%] right-[-5%] shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10"></div>
+
+      <Header />
     </main>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,87 @@
+import 'boxicons/css/boxicons.min.css';
+
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center py-4 px-4 lg:px-20">
+      <h1 className="text-3xl md:text-4x1 lg:text-5x1 font-light m-0">MCODE</h1>
+
+      {/* Desktop Navigation */}
+      <nav className="hidden md:flex items-center gap-12">
+        <a
+          className="text-base tracking-wider transition-colors hover:text-gray-300 z-50"
+          href="#"
+        >
+          COMPANY
+        </a>
+
+        <a
+          className="text-base tracking-wider transition-colors hover:text-gray-300 z-50"
+          href="#"
+        >
+          FEATURES
+        </a>
+
+        <a
+          className="text-base tracking-wider transition-colors hover:text-gray-300 z-50"
+          href="#"
+        >
+          RESOURCES
+        </a>
+
+        <a
+          className="text-base tracking-wider transition-colors hover:text-gray-300 z-50"
+          href="#"
+        >
+          DOCS
+        </a>
+      </nav>
+
+      <button className="hidden md:block bg-[#a7a7a7] text-black py-3 px-8 rounded-full border-none font-medium transition-all duration-500 hover:bg-white cursor-pointer z-50">
+        SIGNIN
+      </button>
+
+      {/* Mobile Menu Button - Visible only on Mobile */}
+      <button className="md:hidden text-3xl p-2 z-50">
+        <i class="bx bx-menu"></i>
+      </button>
+
+      {/* Mobile Menu - Hidden by default */}
+      <div
+        id="mobileMenu"
+        className="hidden fixed top-16 bottom-0 right-0 left-0 p-5 md:hidden z-40 bg-black bg-opacity-70 backdrop-blur- md"
+      >
+        <nav className="flex flex-col gap-6 items-center">
+          <a
+            className="text-base tracking-wider transition-colors hover:text-gray-300 z-50"
+            href="#"
+          >
+            COMPANY
+          </a>
+
+          <a
+            className="text-base tracking-wider transition-colors hover:text-gray-300 z-50"
+            href="#"
+          >
+            FEATURES
+          </a>
+
+          <a
+            className="text-base tracking-wider transition-colors hover:text-gray-300 z-50"
+            href="#"
+          >
+            RESOURCES
+          </a>
+
+          <a
+            className="text-base tracking-wider transition-colors hover:text-gray-300 z-50"
+            href="#"
+          >
+            DOCS
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
